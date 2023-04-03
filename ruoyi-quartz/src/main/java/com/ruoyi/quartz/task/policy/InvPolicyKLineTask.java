fb7fcbd9fe19487dadbc9f3c3f1009b50d14bae9
yay <factorycode@sina.com>
2023-04-03 12:50:50 +0800

    代码优化

--- ruoyi-quartz/src/main/java/com/ruoyi/quartz/task/policy/InvPolicyKLineTask.java
+++ ruoyi-quartz/src/main/java/com/ruoyi/quartz/task/policy/InvPolicyKLineTask.java
@@ -9,23 +9,24 @@ import com.ruoyi.investment.domain.InvCompany;
 import com.ruoyi.investment.domain.InvKLine;
 import com.ruoyi.investment.mapper.InvCompanyMapper;
 import com.ruoyi.investment.mapper.InvKLineMapper;
-import com.ruoyi.investment.mapper.InvStockMapper;
-import com.ruoyi.quartz.async.data.InvDataKLineAsyncTask;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
 @Slf4j
 @Component
 public class InvPolicyKLineTask {
-    @Resource
-    private InvStockMapper invStockMapper;
-
-
     @Resource
     private InvCompanyMapper invCompanyMapper;
     @Resource
@@ -64,26 +65,85 @@ public class InvPolicyKLineTask {
         //筛选出全部正常开市的日期
         Date reportDate = DateUtils.parseDate("1999-12-31");
         StringBuilder sb = new StringBuilder();
-        sb.append("select report_date from inv_k_line where security_code in(");
+        sb.append("select security_code, up_down_range from inv_k_line where security_code in(");
         for (InvCompany invCompany : companyList) {
             sb.append("'").append(invCompany.getCode()).append("'").append(",");
         }
 
-        List<String> dataList = new ArrayList<>();
+        List<String> upDownRangeList = new ArrayList<>();
         while (DateUtils.differentDaysByMillisecond(reportDate, new Date()) > 0) {
             reportDate = DateUtils.dateAddOne(reportDate);
             String dataStr = DateUtils.dateTime(reportDate);
             String sq = sb.toString();
             sq = sq.substring(0, sq.length() - 1);
-            sq += ") and report_date='" + dataStr + "' order by report_date, security_code";
+            sq += ") and report_date='" + dataStr + "' order by security_code";
             List<InvKLine> invKLines = invKLineMapper.commonSelect(sq);
             if (invKLines.size() == companyList.size()) {
-                dataList.add(dataStr);
+                String lib = "-0,0";
+                StringBuilder list = new StringBuilder();
+                int time = 0;
+                for (InvKLine invKLine : invKLines) {
+                    if ("000002".equals(invKLine.getSecurityCode())) {
+                        if (invKLine.getUpDownRange() > 0) {
+                            lib = "-1,0";
+                        }
+                    } else {
+                        if (time<3){
+                            if (invKLine.getUpDownRange() > 0) {
+                                list.append("1,");
+                            } else {
+                                list.append("0,");
+                            }
+                            time++;
+                        }else{
+                            break;
+                        }
+                    }
+                }
+                ;
+                upDownRangeList.add(list.toString().substring(0,list.length()-1) + lib);
             }
         }
+        writeTrainData("/Users/yay/WorkSpace/RuoYi/RuoYi-master/policyFile/KLine/KLine.txt", upDownRangeList);
+    }
 
 
-        System.out.println();
+    /**
+     * 生成训练数据TXT文件
+     */
+    private void writeTrainData(String fileName, List<String> upDownRangeList) {
+        BufferedWriter bw = null;
+        try {
+            File file = new File(fileName);
+            File paFile = file.getParentFile();
+            // 判断文件夹是否存在
+            if (!paFile.exists()) {
+                paFile.mkdirs();
+            }
+            // 判断文件是否存在
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+
+            // 遍历写入
+            bw = new BufferedWriter(new FileWriter(file));
+            for (String content : upDownRangeList) {
+                bw.write(content);
+                bw.write(System.getProperty("line.separator"));
+            }
+            bw.flush();
+            bw.close();
+        } catch (Exception e) {
+            log.error(">>>异常:", e);
+        } finally {
+            if (null != bw) {
+                try {
+                    bw.close();
+                } catch (IOException e) {
+                }
+            }
+        }
     }
 
 }
